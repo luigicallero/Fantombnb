1fc1da3a5c8aef0f7a9fb3ac2d5cffd3f2afbcb8
luigicallero <luigicallero@gmail.com>
2023-02-14 15:08:42 -0300

    commenting lines used to connecto to Fantom chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,13 @@ module.exports = {
     fantom_mainnet: {
       url: `https://rpcapi.fantom.network`,
       chainId: 250,
-      accounts: [`0x${PRIVATE_KEY}`]
+      //accounts: [`0x${PRIVATE_KEY}`]
     },
     fantom_testnet: {
       //url: `https://rpc.testnet.fantom.network`,
       url: `https://rpcapi-tracing.testnet.fantom.network`,
       chainId: 4002,
-      accounts: [`0x${PRIVATE_KEY}`]
+      //accounts: [`0x${PRIVATE_KEY}`]
     }
   }
 };
