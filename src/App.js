d7712bdb51622db265127eff1bf1d4134ceac531
Indiana Meregone <juanmeregone@gmail.com>
2023-02-22 10:04:14 -0300

    las vistas de la pagina

--- src/App.js
+++ src/App.js
@@ -63,33 +63,34 @@ function App() {
     <div>
       <Navigation account={account} setAccount={setAccount} />
       <br></br>
-      <Search />
+      <div id="container">
+        <Search />
 
-      <div className='cards__section'>
+        <div className='cards__section'>
 
-        <h3>Homes For You</h3>
+          <h3>Homes For You</h3>
 
-        <hr />
+          <hr />
 
-        <div className='cards'>
-          {homes.map((home, index) => (
-            <div className='card' key={index} onClick={() => togglePop(home)}>
-              <div className='card__image'>
-                <img src={home.image} alt="Home" />
+          <div className='cards'>
+            {homes.map((home, index) => (
+              <div className='card' key={index} onClick={() => togglePop(home)}>
+                <div className='card__image'>
+                  <img src={home.image} alt="Home" />
+                </div>
+                <div className='card__info'>
+                  <h4>{home.attributes[0].value} ETH</h4>
+                  <p>
+                    <strong>{home.attributes[2].value}</strong> bds |
+                    <strong>{home.attributes[3].value}</strong> ba |
+                    <strong>{home.attributes[4].value}</strong> sqft
+                  </p>
+                  <p>{home.address}</p>
+                </div>
               </div>
-              <div className='card__info'>
-                <h4>{home.attributes[0].value} ETH</h4>
-                <p>
-                  <strong>{home.attributes[2].value}</strong> bds |
-                  <strong>{home.attributes[3].value}</strong> ba |
-                  <strong>{home.attributes[4].value}</strong> sqft
-                </p>
-                <p>{home.address}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-
       </div>
 
       {toggle && (
